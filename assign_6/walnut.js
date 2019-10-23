661d069c70af350c695b77c51b1fd8d5cbd32826
Amy Lu <amyyejun@amy-lus-MacBook-Pro-2.local>
2019-10-23 18:56:26 -0400

    Update walnut.js

--- assign_6/walnut.js
+++ assign_6/walnut.js
@@ -15,13 +15,13 @@ function imgClick(){
     var thisthumb = document.getElementById(this.id);
    // largeOne.src = dict[thisthumb.getAttribute('src')]
    
-    if (thisthumb.getAttribute('src')== "/Images/walnut_thumbnail_1.png") {
-        largeOne.src = "/Images/Walnut.jpg";
+    if (thisthumb.getAttribute('src')== "Images/walnut_thumbnail_1.png") {
+        largeOne.src = "Images/Walnut.jpg";
         // document.getElementById('thumbnailOne').style.border="2px solid black";
         // document.getElementById('thumbnailOne').style.borderRadius="40px";
         // document.getElementById('thumbnailTwo').style.border="hidden";
-    } else if (thisthumb.getAttribute('src')== "/Images/walnut_thumbnail_2.png")
-        largeOne.src = "/Images/Caramel Pecan.jpg";
+    } else if (thisthumb.getAttribute('src')== "Images/walnut_thumbnail_2.png")
+        largeOne.src = "Images/Caramel Pecan.jpg";
         // document.getElementById('thumbnailOne').style.border="hidden";
         // document.getElementById('thumbnailTwo').style.border="2px solid black";
         // document.getElementById('thumbnailTwo').style.borderRadius="40px";
@@ -39,25 +39,25 @@ function borderClick() {
     console.log(this.id)
     var thisborder = document.getElementById(this.id);
 
-    if (thisborder.getAttribute('src') == "/Images/None.png") {
+    if (thisborder.getAttribute('src') == "Images/None.png") {
         document.getElementById('clickBorderOne').style.border="2px solid black";
         document.getElementById('clickBorderOne').style.borderRadius="30px";
         document.getElementById('clickBorderTwo').style.border="hidden";
         document.getElementById('clickBorderThree').style.border="hidden";
         document.getElementById('clickBorderFour').style.border="hidden";
-    }  if (thisborder.getAttribute('src') == "/Images/Sugar-milk.png") {
+    }  if (thisborder.getAttribute('src') == "Images/Sugar-milk.png") {
         document.getElementById('clickBorderTwo').style.border="2px solid black";
         document.getElementById('clickBorderTwo').style.borderRadius="30px";
         document.getElementById('clickBorderOne').style.border="hidden";
         document.getElementById('clickBorderThree').style.border="hidden";
         document.getElementById('clickBorderFour').style.border="hidden";
-    } if (thisborder.getAttribute('src') == "/Images/Vanilla-Milk.png") {
+    } if (thisborder.getAttribute('src') == "Images/Vanilla-Milk.png") {
         document.getElementById('clickBorderOne').style.border="hidden";
         document.getElementById('clickBorderTwo').style.border="hidden";
         document.getElementById('clickBorderThree').style.border="2px solid black";
         document.getElementById('clickBorderThree').style.borderRadius="30px";
         document.getElementById('clickBorderFour').style.border="hidden";
-    } if (thisborder.getAttribute('src') == "/Images/Double-Chocolate.png") {
+    } if (thisborder.getAttribute('src') == "Images/Double-Chocolate.png") {
         document.getElementById('clickBorderOne').style.border="hidden";
         document.getElementById('clickBorderTwo').style.border="hidden";
         document.getElementById('clickBorderThree').style.border="hidden";
